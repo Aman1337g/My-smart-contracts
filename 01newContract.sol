58cf24134a62c52be7666ac24f7b84404b05fa15
Aman Kumar Gupta <97891757+Aman1337g@users.noreply.github.com>
2022-07-24 09:45:49 +0530

    Create 01newContract.sol

---
+++ 01newContract.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.5.0; //this compiler will be selected and "^" is for upward compatibility
+
+contract MyContract {
+    string public myString = "Hello World"; // visibiltiy = public here
+}
