41f6a59328941ba7e447a80dd5fbdde6538c0ba5
Aman Kumar Gupta <aman65144kumar@gmail.com>
2022-07-24 13:41:40 +0530

    Update 02variables.sol

--- 02variables.sol
+++ 02variables.sol
@@ -9,7 +9,7 @@ contract variables {
     string public mystr; //non-indexable unlike C, C++    
 
     //uint = unsigned integer, uint8 contains 2^8 = 256 characters (0 to 255) 
-    //int = integer , int8 = 256 charactors (-128 to 127)
+    //int = integer , int8 = 256 charactors (-128 to +127)
     //uint8 is more preferable than uint88 act it will use less gas
     //all integer are initialised with 0 and string with null and boolean type with false
 
