002b4ba42485fb0af546e89f3cd7bf2394e3df9b
Aman Kumar Gupta <aman65144kumar@gmail.com>
2022-07-24 13:38:42 +0530

    Update 03address.sol

--- 03address.sol
+++ 03address.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.5.0;
 
 contract addr {
-    address public myAdd; // Two important functions of address variable - 1.For sending funds 2.For conferences
-    ufixed128x18 public myfix; //for fix point numbers
+    address public myAdd; // Two important functions of address variable - 1.For sending funds 2.For account balances
+    ufixed128x18 public myfix; //for fix point numbers - used for decimal numbers. Not applied on Blockchain.
     constructor() public {
         myAdd = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4; //for initializing myAdd(account) otherwise initialised with 0x0000000000000000000000000000000000000000
     }
