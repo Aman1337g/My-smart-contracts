e219dd7b5d1c5d2778902fcb9a4e6948d3aa75b3
Aman Kumar Gupta <aman65144kumar@gmail.com>
2022-07-26 08:28:18 +0530

    Added import.sol for importing from github and directory

---
+++ import.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.6.0;
+
+contract owned { // parent contract
+    address public owner;
+    constructor()public {   // deployed at compile time
+        owner = msg.sender;  //msg.sender will give us the address who has started the transaction
+    }
+    modifier onlyOwner() {
+        require(owner == msg.sender, "You are not the owner");
+        _;  //Placeholder
+    }
+}
