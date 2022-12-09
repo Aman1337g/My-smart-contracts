b80fdab1266a1045d5bcc565f0e3bcfe0a5e876a
Aman Kumar Gupta <97891757+Aman1337g@users.noreply.github.com>
2022-12-09 23:15:06 +0530

    Update 01newContract.sol

--- 01newContract.sol
+++ 01newContract.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0; //(PRE-COMPILER LINE)this compiler will be selected and "^" is for upward compatibility
+pragma solidity ^0.5.0; //(PRE-COMPILER LINE)this compiler will be selected and "^" is for upward compatibility 
 
 contract MyContract {
     string public myString = "Hello World"; // visibiltiy = public here
